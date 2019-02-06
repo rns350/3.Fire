7eb962f24372bd88592c9aedd0c728ba58e0ceb6
rns350 <rns350@nyu.edu>
2019-02-06 09:15:58 -0500

    Bard class started

---
+++ Bard.java
@@ -0,0 +1,10 @@
+public Class Bard Extends gameClass{
+    fortLevels = [0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6];
+    refLevels = [2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12];
+    willLevels = [2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12];
+    attackLevels = [0, 1, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9, 9, 10, 11, 12, 12, 13, 14, 15];
+
+   Bard(int level){
+       this.level = level;
+   }
+}
